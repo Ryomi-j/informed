7a895b914ed01bee44330dd476f38fef0a69b4c8
Ryomi <ryomi.j78@gmail.com>
2025-01-03 21:09:45 +0900

    make navigation bar

---
+++ src/lib/ui/nav.tsx
@@ -0,0 +1,53 @@
+import { Searchbar } from "./searchbar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/lib/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/lib/ui/avatar";
+import { ChevronDownIcon } from "lucide-react";
+import Link from "next/link";
+import { DROPDOWN_ROUTES } from "../constants/routes";
+
+function Nav() {
+  return (
+    <div className="w-full flex justify-between items-center p-4 gap-2">
+      {/* Logo */}
+      <h1 className="text-2xl font-bold">Informed</h1>
+
+      {/* Searchbar */}
+      <Searchbar placeholder="Search" iconPosition="left" className="w-2/3" />
+
+      {/* User Dropdown Menu */}
+      <DropdownMenu>
+        <DropdownMenuTrigger className="group flex items-center gap-2 focus:outline-none">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            {/* TODO: 사용자 이미지 없을 때 대체 텍스트 수정 */}
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <p className="text-sm font-medium">Angela</p>
+          <ChevronDownIcon className="w-4 h-4 text-gray-500 group-data-[state=open]:rotate-180 transition-transform duration-200" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {Object.entries(DROPDOWN_ROUTES).map(([key, value]) => (
+            <DropdownMenuItem>
+              <Link href={value} className="font-medium">
+                {key}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+export default Nav;
