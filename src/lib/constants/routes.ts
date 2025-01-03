0ea86a1396ee05c3df236e1a37979a718ca24f75
Ryomi <ryomi.j78@gmail.com>
2025-01-03 21:08:18 +0900

    declare default routes

---
+++ src/lib/constants/routes.ts
@@ -0,0 +1,12 @@
+export const ROUTES = {
+  home: "/",
+  profile: "/profile",
+  notifications: "/notifications",
+  keywords: "/keywords",
+};
+
+export const DROPDOWN_ROUTES = {
+  Profile: "/profile",
+  Notifications: "/notifications",
+  Keywords: "/keywords",
+};
