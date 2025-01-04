3b27aa9bc538d1f5052f5675ee93490662cf2bde
Ryomi <ryomi.j78@gmail.com>
2025-01-04 15:34:36 +0900

    make category tab in main page

---
+++ src/lib/components/categoryTabs.tsx
@@ -0,0 +1,31 @@
+import { TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { DEFAULT_CATEGORY } from "../constants/category";
+import Link from "next/link";
+import { clsx } from "clsx";
+
+export function CategoryTabs({
+  selectedCategory,
+}: {
+  selectedCategory: string;
+}) {
+  return (
+    <TabsList className="flex gap-5">
+      {Object.keys(DEFAULT_CATEGORY).map((category) => (
+        <Link href={`?category=${category}`} key={category} className="w-fit">
+          <TabsTrigger
+            key={category}
+            value={category}
+            className={clsx(
+              "w-full",
+              selectedCategory === category
+                ? "text-black border-b-2 border-black"
+                : "text-gray-300"
+            )}
+          >
+            {DEFAULT_CATEGORY[category as keyof typeof DEFAULT_CATEGORY]}
+          </TabsTrigger>
+        </Link>
+      ))}
+    </TabsList>
+  );
+}
