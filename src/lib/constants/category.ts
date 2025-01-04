0ab4eb9889b3f4d00951b51b4cb12c98afc5c0ab
Ryomi <ryomi.j78@gmail.com>
2025-01-04 15:33:50 +0900

    declare DEFAULT CATEGORY

---
+++ src/lib/constants/category.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_CATEGORY = {
+  technology: "테크놀로지",
+  science: "과학",
+  health: "건강",
+  business: "비즈니스",
+  entertainment: "엔터테인먼트",
+  sports: "스포츠",
+  politics: "정치",
+};
