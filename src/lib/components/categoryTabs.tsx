35055af4bec8b51749b729c900633f49e4a361a1
Ryomi <ryomi.j78@gmail.com>
2025-01-04 17:14:40 +0900

    add scroll function in CategoryTabs

--- src/lib/components/categoryTabs.tsx
+++ src/lib/components/categoryTabs.tsx
@@ -2,6 +2,8 @@ import { TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import { DEFAULT_CATEGORY } from "../constants/category";
 import Link from "next/link";
 import { clsx } from "clsx";
+import { ScrollArea } from "../ui/scroll-area";
+import { ScrollBar } from "../ui/scroll-area";
 
 export function CategoryTabs({
   selectedCategory,
@@ -9,22 +11,29 @@ export function CategoryTabs({
   selectedCategory: string;
 }) {
   return (
-    <TabsList className="flex gap-5 max-w-screen-xl mx-auto">
-      {Object.keys(DEFAULT_CATEGORY).map((category) => (
-        <Link href={`?category=${category}`} key={category} className="w-fit">
-          <TabsTrigger
-            value={category}
-            className={clsx(
-              "w-full pb-2",
-              selectedCategory === category
-                ? "text-black border-b-4 border-black"
-                : "text-gray-500"
-            )}
+    <ScrollArea className="w-full max-w-screen-xl mx-auto">
+      <TabsList className="flex gap-5">
+        {Object.keys(DEFAULT_CATEGORY).map((category) => (
+          <Link
+            href={`?category=${category}`}
+            key={category}
+            className="w-fit min-w-fit"
           >
-            {DEFAULT_CATEGORY[category as keyof typeof DEFAULT_CATEGORY]}
-          </TabsTrigger>
-        </Link>
-      ))}
-    </TabsList>
+            <TabsTrigger
+              value={category}
+              className={clsx(
+                "w-full pb-2",
+                selectedCategory === category
+                  ? "text-black border-b-4 border-black"
+                  : "text-gray-500"
+              )}
+            >
+              {DEFAULT_CATEGORY[category as keyof typeof DEFAULT_CATEGORY]}
+            </TabsTrigger>
+          </Link>
+        ))}
+      </TabsList>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 }
