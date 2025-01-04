f2ebc8b4ad2db7e37efd33f2e8e9d4645f189366
Ryomi <ryomi.j78@gmail.com>
2025-01-04 16:05:41 +0900

    add NewsCard to TabContent

--- src/lib/components/tabContent.tsx
+++ src/lib/components/tabContent.tsx
@@ -1,7 +1,14 @@
 import { TabsContent } from "@/lib/ui/tabs";
+import { NewsCard } from "./newsCard";
 
 export function TabContent({ category }: { category: string }) {
   // TODO: 카테고리 데이터 가져오기
-  // const categoryData = ''
-  return <TabsContent value={category}>{category}</TabsContent>;
+  // const categoryData = await getCategoryData(category)
+  return (
+    <TabsContent value={category} className="grid grid-cols-3 gap-4">
+      <NewsCard category={category} />
+      <NewsCard category={category} />
+      <NewsCard category={category} />
+    </TabsContent>
+  );
 }
