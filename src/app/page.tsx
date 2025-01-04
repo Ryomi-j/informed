f210ec1e4eb4dd7935e3bbb1f3a1f782b5f095dd
Ryomi <ryomi.j78@gmail.com>
2025-01-04 16:34:20 +0900

    add refresh button to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { DEFAULT_CATEGORY } from "@/lib/constants/category";
 import { CategoryTabs } from "@/lib/components/categoryTabs";
 import { TabContent } from "@/lib/components/tabContent";
 import { Tabs } from "@/lib/ui/tabs";
+import { Button } from "@/lib/ui/button";
+import { RefreshCwIcon } from "lucide-react";
 
 export default async function Home(props: {
   searchParams: Promise<{ category: string }>;
@@ -14,16 +16,23 @@ export default async function Home(props: {
 
   return (
     <Fragment>
-      <Tabs
-        defaultValue={DEFAULT_CATEGORY.technology}
-        className="w-full"
-      >
+      <Tabs defaultValue={DEFAULT_CATEGORY.technology} className="w-full">
         <div className="w-full bg-white">
           <CategoryTabs selectedCategory={category} />
         </div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <TabContent category={category} />
-        </Suspense>
+        <div className="max-w-screen-xl mx-auto w-full">
+          <div className="flex items-center gap-2 w-full pt-2 text-gray-500">
+            <Button>
+              <RefreshCwIcon className="w-4 h-4" />
+              <span>새로고침</span>
+            </Button>
+            <p>마지막 업데이트: </p>
+            <span>5분 전</span>
+          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <TabContent category={category} />
+          </Suspense>
+        </div>
       </Tabs>
     </Fragment>
   );
--- src/lib/components/tabContent.tsx
+++ src/lib/components/tabContent.tsx
@@ -5,7 +5,7 @@ export function TabContent({ category }: { category: string }) {
   // TODO: 카테고리 데이터 가져오기
   // const categoryData = await getCategoryData(category)
   return (
-    <TabsContent value={category} className="max-w-screen-xl mx-auto grid grid-cols-3 gap-4">
+    <TabsContent value={category} className="grid grid-cols-3 gap-4">
       <NewsCard category={category} />
       <NewsCard category={category} />
       <NewsCard category={category} />
