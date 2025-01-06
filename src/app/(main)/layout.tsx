836b5a04d50f6e9387a979e34f776790de47b3ef
Ryomi <ryomi.j78@gmail.com>
2025-01-06 11:41:13 +0900

    delete unnecessary codes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import clsx from "clsx";
 import Nav from "@/lib/components/nav";
 import { Footer } from "@/lib/components/footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Fragment } from "react";
 
 export default function RootLayout({
   children,
@@ -20,18 +9,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={clsx(
-          "flex flex-col justify-between min-h-screen antialiased bg-gray-100",
-          geistSans.variable,
-          geistMono.variable
-        )}
-      >
-        <Nav />
-        <main className="grow">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <Fragment>
+      <Nav />
+      <main className="grow">{children}</main>
+      <Footer />
+    </Fragment>
   );
 }
