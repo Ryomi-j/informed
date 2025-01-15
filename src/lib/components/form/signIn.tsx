9af26669649757a1818ab1c214773120c590ce73
Ryomi <ryomi.j78@gmail.com>
2025-01-15 16:04:24 +0900

    apply server actions in sign in and out forms

--- src/lib/components/form/signIn.tsx
+++ src/lib/components/form/signIn.tsx
@@ -1,48 +1,96 @@
+"use client";
+
 import { Button } from "@/lib/ui/button";
 import { Input } from "@/lib/ui/input";
 import { MailIcon } from "lucide-react";
 import { Label } from "@/lib/ui/label";
 import { LockIcon } from "lucide-react";
+import { useActionState } from "react";
+import { authenticate } from "@/lib/actions/auth";
+import { Fragment } from "react";
+import { CircleAlertIcon } from "lucide-react";
+import { clsx } from "clsx";
+import { useAuthStore } from "@/lib/store/auth";
+import { useEffect } from "react";
+import { auth } from "@/auth";
 
 export const SignInForm = () => {
+  const setUser = useAuthStore((state) => state.setUser);
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined
+  );
+
+  useEffect(() => {
+    if (!errorMessage) {
+      const fetchUser = async () => {
+        const session = await auth();
+        if (session?.user) {
+          setUser({
+            id: session.user.id!,
+            email: session.user.email!,
+            name: session.user.name!,
+            profile_image: session.user.image!,
+            auth_provider: session.user.provider,
+          });
+        }
+      };
+      fetchUser();
+    }
+  }, [errorMessage, setUser]);
+
   const EMAIL_PLACEHOLDER = "이메일을 입력하세요";
   const PASSWORD_PLACEHOLDER = "비밀번호를 입력하세요";
 
   return (
-    <form className="w-full">
-      <div className="flex flex-col gap-6">
-        <div className="grid gap-2">
-          <Label htmlFor="email">이메일</Label>
-          <div className="flex items-center gap-2 pl-2 border-2 has-[:focus]:border-gray-500 rounded-lg">
-            <MailIcon className="w-4 h-4 text-white fill-gray-500" />
-            <Input
-              className="border-none shadow-none focus:outline-none focus:border-transparent focus:ring-0"
-              id="email"
-              type="email"
-              placeholder={EMAIL_PLACEHOLDER}
-              required
-            />
+    <Fragment>
+      <form className="w-full" action={formAction}>
+        <div className="flex flex-col gap-6">
+          <div className="grid gap-2">
+            <Label htmlFor="email">이메일</Label>
+            <div className="flex items-center gap-2 pl-2 border-2 has-[:focus]:border-gray-500 rounded-lg">
+              <MailIcon className="w-4 h-4 text-white fill-gray-500" />
+              <Input
+                className="border-none shadow-none focus:outline-none focus:border-transparent focus:ring-0"
+                id="email"
+                type="email"
+                name="email"
+                placeholder={EMAIL_PLACEHOLDER}
+                required
+              />
+            </div>
           </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">비밀번호</Label>
+            </div>
+            <div className="flex items-center gap-2 pl-2 border-2 has-[:focus]:border-gray-500 rounded-lg">
+              <LockIcon className="w-4 h-4 fill-gray-500 text-white" />
+              <Input
+                className="border-none shadow-none focus:outline-none focus:border-transparent focus:ring-0"
+                id="password"
+                type="password"
+                name="password"
+                placeholder={PASSWORD_PLACEHOLDER}
+                required
+              />
+            </div>
+          </div>
+          {/* TODO: 화면 흔들림 수정 필요 */}
+          <div
+            className={clsx(
+              "flex items-center gap-2",
+              errorMessage ? "opacity-100" : "opacity-0"
+            )}
+          >
+            <CircleAlertIcon className="h-5 w-5 text-red-500" />
+            <p className="text-red-500">{errorMessage}</p>
+          </div>
+          <Button className="w-full" disabled={isPending}>
+            로그인
+          </Button>
         </div>
-        <div className="grid gap-2">
-          <div className="flex items-center">
-            <Label htmlFor="password">비밀번호</Label>
-          </div>
-          <div className="flex items-center gap-2 pl-2 border-2 has-[:focus]:border-gray-500 rounded-lg">
-            <LockIcon className="w-4 h-4 fill-gray-500 text-white" />
-            <Input
-              className="border-none shadow-none focus:outline-none focus:border-transparent focus:ring-0"
-              id="password"
-              type="password"
-              placeholder={PASSWORD_PLACEHOLDER}
-              required
-            />
-          </div>
-        </div>
-        <Button type="submit" className="w-full">
-          로그인
-        </Button>
-      </div>
-    </form>
+      </form>
+    </Fragment>
   );
 };
--- src/lib/components/form/signUp.tsx
+++ src/lib/components/form/signUp.tsx
@@ -1,45 +1,83 @@
+"use client";
+
+import { signUp } from "@/lib/actions/auth";
+import { State } from "@/lib/types/form";
 import { Button } from "@/lib/ui/button";
 import { Input } from "@/lib/ui/input";
 import { Label } from "@/lib/ui/label";
 import Link from "next/link";
+import { useActionState } from "react";
 
 export const SignUpForm = () => {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction] = useActionState(signUp, initialState);
+
   const EMAIL_PLACEHOLDER = "이메일 주소";
   const PASSWORD_PLACEHOLDER = "비밀번호";
   const PASSWORD_CHECK_PLACEHOLDER = "비밀번호 확인";
 
   return (
-    <form className="w-full">
+    <form className="w-full" action={formAction}>
       <div className="flex flex-col gap-6">
         <div className="grid gap-2">
           <Label htmlFor="email">이메일</Label>
           <Input
+            name="email"
             className="focus:border-gray-500"
             id="email"
             type="email"
             placeholder={EMAIL_PLACEHOLDER}
             required
+            aria-describedby="email-error"
           />
+          <div id="email-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.email &&
+              state.errors.email.map((error: string) => (
+                <p className="text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password">비밀번호</Label>
           <Input
+            name="password"
             className="focus:border-gray-500"
             id="password"
             type="password"
             placeholder={PASSWORD_PLACEHOLDER}
             required
+            aria-describedby="password-error"
           />
+          <div id="password-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.password &&
+              state.errors.password.map((error: string) => (
+                <p className="text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
         <div className="grid gap-2">
           <Label htmlFor="password-check">비밀번호 확인</Label>
           <Input
+            name="confirmPassword"
             className="focus:border-gray-500"
             id="password-check"
             type="password"
             placeholder={PASSWORD_CHECK_PLACEHOLDER}
             required
+            aria-describedby="password-check-error"
           />
+          <div id="password-check-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.confirmPassword &&
+              state.errors.confirmPassword.map((error: string) => (
+                <p className="text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
         {/* 약관 동의 */}
         {/* <div className="flex items-center gap-2">
