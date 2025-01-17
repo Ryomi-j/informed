2af32a3e4743987f9c5f4a9dae194d0cbea8c956
Ryomi <ryomi.j78@gmail.com>
2025-01-17 22:27:02 +0900

    update news contents conponents

--- src/lib/components/newsCard.tsx
+++ src/lib/components/newsCard.tsx
@@ -1,43 +1,52 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/lib/ui/card";
 import Image from "next/image";
-import { DEFAULT_CATEGORY } from "../constants/category";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import { News } from "../types/news";
+import { formatDate } from "../utils/date";
+import { ROUTES } from "../constants/routes";
 
-export function NewsCard({ category }: { category: string }) {
+export function NewsCard({
+  category,
+  content,
+}: {
+  category: string;
+  content: News;
+}) {
   return (
     <Card className="w-[400px] h-[396px]">
-      <CardHeader>
-        <Image
-          src="/article.png"
-          alt={`${category} news item`}
-          width={400}
-          height={198}
-        />
+      <CardHeader className="h-3/6">
+        {content.thumbnail_small ? (
+          <Image
+            src={content.thumbnail_small}
+            alt={`${category} news item`}
+            width={400}
+            height={150}
+            className="object-cover overflow-hidden rounded-lg"
+            loading="lazy"
+          />
+        ) : (
+          <div className="w-full h-full flex justify-center items-center bg-gray-200 rounded-lg">
+            <p className="text-gray-500">No Image</p>
+          </div>
+        )}
       </CardHeader>
-      <CardContent className="flex flex-col gap-3">
+      <CardContent className="flex flex-col gap-3 h-2/6">
         <div className="flex items-center gap-2 text-gray-500">
-          <span>{DEFAULT_CATEGORY.technology}</span>
+          <span className="truncate">{content?.source?.name ?? ""}</span>
           <span>•</span>
-          {/* TODO: 시간 추가 */}
-          <span>2시간 전</span>
+          <span>{formatDate(content?.date ?? "")}</span>
         </div>
-
-        <CardTitle>삼성전자, 새로운 AI 칩 개발 발표</CardTitle>
-        <CardDescription className="text-gray-500 line-clamp-2">
-          삼성전자가 차세대 AI 반도체 개발 계획을 발표했습니다. 이 번 칩은 기존
-          제품 대비 성능이 2배 향상되었으며asfasdfasfasdf...
-        </CardDescription>
+        <CardTitle className="leading-7	">{content?.title ?? ""}</CardTitle>
       </CardContent>
-      <CardFooter>
-        <Link href="/">
+      <CardFooter className="h-1/6">
+        <Link href={content?.link ?? ROUTES.HOME} target="_blank">
           <Button variant="outline">자세히 보기 →</Button>
         </Link>
       </CardFooter>
--- src/lib/components/tabContent.tsx
+++ src/lib/components/tabContent.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import { TabsContent } from "@/lib/ui/tabs";
 import { NewsCard } from "./newsCard";
+import { News } from "../types/news";
+import { useNews } from "../api/news";
+import { Loading } from "./Loading";
 
 export function TabContent({ category }: { category: string }) {
-  // TODO: 카테고리 데이터 가져오기
-  // const categoryData = await getCategoryData(category)
+  const news = useNews(category);
+  
+  if (!news) return <Loading />;
+
   return (
     <TabsContent value={category} className="grid grid-cols-3 gap-4">
-      <NewsCard category={category} />
-      <NewsCard category={category} />
-      <NewsCard category={category} />
+      {news.map(
+        (content: News) =>
+          content.title && (
+            <NewsCard
+              key={content.position}
+              content={content}
+              category={category}
+            />
+          )
+      )}
     </TabsContent>
   );
 }
