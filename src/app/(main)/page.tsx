4b996c83dbf1e08173a80d8da3a408b7b495e598
Ryomi <ryomi.j78@gmail.com>
2025-01-20 20:59:59 +0900

    delete unnecessary code

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,7 +4,7 @@ import { KeywordTabs } from "@/lib/components/keywordTabs";
 import { TabContent } from "@/lib/components/tabContent";
 import { Tabs } from "@/lib/ui/tabs";
 import { Button } from "@/lib/ui/button";
-import { RefreshCwIcon, SettingsIcon } from "lucide-react";
+import { SettingsIcon } from "lucide-react";
 import Link from "next/link";
 import QueryProvider from "@/lib/components/queryProvider";
 import { ROUTES } from "@/lib/constants/routes";
@@ -21,11 +21,10 @@ export default async function Home(props: {
         <div className="w-full bg-white">
           <KeywordTabs selectedKeyword={keyword} />
         </div>
-        <div className="max-w-screen-xl mx-auto w-full">
-          <QueryProvider>
-            <TabContent />
-          </QueryProvider>
-        </div>
+
+        <QueryProvider>
+          <TabContent />
+        </QueryProvider>
       </Tabs>
       <Link href={ROUTES.KEYWORDS} className="fixed bottom-4 right-10 w-8 h-8">
         <Button>
